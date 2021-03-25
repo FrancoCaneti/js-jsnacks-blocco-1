074fe785071dbe2675408cbcaa32e742f91bda49
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-25 15:20:09 +0100

    snack 1

---
+++ script.js
@@ -0,0 +1,33 @@
+/*Snack 1
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+//Esegui questo programma in due versioni, con il for e con il while
+
+// eseguito con il for
+
+var richieste = 5
+var somma = 0
+
+for ( var i = 1; i <= richieste; i++){
+ var numero = parseInt(prompt('inserisci il numero' + i + 'di' + richieste) );
+console.log(numero)
+
+somma += numero;
+}
+console.log('totale',somma);  */
+
+
+
+//eseguito con il while
+
+/*var contatore = 1;
+while (contatore <= richieste ) {
+    var numero = parseInt(prompt('Inserisci numero ' + contatore +'di' + richieste));
+    console.log(numero);
+
+    somma += numero;
+    contatore++;
+}
+ console.log('somma:',somma);*/
+
+
