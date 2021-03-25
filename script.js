224eb2ead0db53f563ff87e680192d69ee136d68
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-25 15:37:33 +0100

    snack 3

--- script.js
+++ script.js
@@ -31,3 +31,40 @@ while (contatore <= richieste ) {
  console.log('somma:',somma);*/
 
 
+//snack 2
+/*Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+var numero = parseInt(prompt('Inserisci un numero '));
+
+
+if ( numero % 2 === 0){
+    console.log('numero:',numero);
+  } else {
+      console.log(++numero);
+  
+  }                                                      */
+
+/*Snack 3.
+//Generatore di “nomi cognomi” casuali: 
+//prendendo una lista di nomi e una lista di cognomi, 
+// generare una falsa lista di 3 invitati.
+
+//lista nomi
+var arrayNomi = ['Aldo', 'Giovanni', 'Giacomo','Filippo', 'Lorenzo'];
+console.log('lista nomi:',arrayNomi);
+
+
+//lista cognomi
+var arrayCognomi = ['Bianchi', 'Neri', 'Rossi','Gialli', 'Verdi'];
+console.log('lista cognomi:',arrayCognomi);
+ 
+var listaFake = []
+
+for (var i = 0; i < 3; i++){
+
+ var nomeRandom = Math.floor(Math.random() * arrayNomi.length);
+ var cognomeRandom = Math.floor(Math.random() * arrayCognomi.length);
+
+listaFake = arrayNomi[nomeRandom] + ' ' + arrayCognomi[cognomeRandom];
+console.log(listaFake);
+}   */
+ 
